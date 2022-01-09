16364844967dc1ac909089c60245092d3fd2b0e1
Derebeyi <6437707+batuhanyus@users.noreply.github.com>
2022-01-09 22:05:55 +0300

    39.3 sec, 367 mb

--- Program.cs
+++ Program.cs
@@ -24,16 +24,27 @@ namespace Challenge1
             Console.ReadKey();
         }
 
-        private const int SAMPLE_SIZE = 10000000;       
+        private const int SAMPLE_SIZE = 10000000;
         private static IEnumerable<SummarizedSalesData> Challenge1Solution(IEnumerable<SalesData> items)
         {
             //*****README*****
             //Hi, there. 
 
+            
 
-            Dictionary<string, SummarizedSalesData> summaryHolder = new();
+            Dictionary<int, SummarizedSalesData> summaryHolder = new();
+            
 
-            //To parse data's week data
+            //Dictionary<string, int> brandCodes = new();
+            //Dictionary<string, int> storeCodes = new();
+
+            //string[] brandCodes = new string[10];
+            //string[] storeCodes = new string[100];
+
+            List<string> brandCodes = new List<string>();
+            List<string> storeCodes = new List<string>();
+
+            //To parse data's week data 
             CultureInfo ci = new CultureInfo("en-UK");
             Calendar calendar = ci.Calendar;
             CalendarWeekRule calendarWeekRule = ci.DateTimeFormat.CalendarWeekRule;
@@ -46,8 +57,20 @@ namespace Challenge1
                 int weekNumber = dt.Year * 100; //Ex: 202100
                 weekNumber += calendar.GetWeekOfYear(dt, calendarWeekRule, firstDayOfWeek);
 
+                //Check if brand and store codes have their theCode parts. If not, create.
+                if(!brandCodes.Contains(salesData.BrandId))
+                    brandCodes.Add(salesData.BrandId);
+                if (!storeCodes.Contains(salesData.StoreId))
+                    storeCodes.Add(salesData.StoreId);
+
                 //Create the "theCode".
-                string theCode = $"{salesData.BrandId}{salesData.StoreId}{salesData.CompanyId.ToString()}{salesData.ProductId.ToString()}{weekNumber.ToString()}";
+                int theCode = 0;
+                theCode += (dt.Year - 2020) * 100000000;
+                theCode += calendar.GetWeekOfYear(dt, calendarWeekRule, firstDayOfWeek) * 1000000;
+                theCode += brandCodes.IndexOf(salesData.BrandId) * 100000;
+                theCode += salesData.CompanyId * 10000;
+                theCode += storeCodes.IndexOf(salesData.StoreId) * 100;
+                theCode += salesData.ProductId;
 
                 //Check if we have an entry for theCode, if not create.
                 if (!summaryHolder.ContainsKey(theCode))
@@ -59,7 +82,7 @@ namespace Challenge1
                     ssd.CompanyId = salesData.CompanyId;
                     ssd.BrandId = salesData.BrandId;
                     ssd.StoreId = salesData.StoreId;
-                    ssd.WeekNumber = weekNumber;                    
+                    ssd.WeekNumber = weekNumber;
 
                     summaryHolder.Add(theCode, ssd);
                 }
@@ -72,6 +95,56 @@ namespace Challenge1
             //Our work is done. Now return the correct data.
             return summaryHolder.Select(a => a.Value).ToList();
         }
+
+
+
+        //private static IEnumerable<SummarizedSalesData> Challenge1Solution(IEnumerable<SalesData> items)
+        //{
+        //    //*****README*****
+        //    //Hi, there. 
+
+
+        //    Dictionary<string, SummarizedSalesData> summaryHolder = new();
+
+        //    //To parse data's week data
+        //    CultureInfo ci = new CultureInfo("en-UK");
+        //    Calendar calendar = ci.Calendar;
+        //    CalendarWeekRule calendarWeekRule = ci.DateTimeFormat.CalendarWeekRule;
+        //    DayOfWeek firstDayOfWeek = ci.DateTimeFormat.FirstDayOfWeek;
+
+        //    foreach (var salesData in items)
+        //    {
+        //        //Parse the SalesDate to requested format.
+        //        DateTime dt = DateTime.ParseExact(salesData.SalesDate, "yyyy-MM-dd", ci);
+        //        int weekNumber = dt.Year * 100; //Ex: 202100
+        //        weekNumber += calendar.GetWeekOfYear(dt, calendarWeekRule, firstDayOfWeek);
+
+        //        //Create the "theCode".
+        //        string theCode = $"{salesData.BrandId}{salesData.StoreId}{salesData.CompanyId.ToString()}{salesData.ProductId.ToString()}{weekNumber.ToString()}";
+
+        //        //Check if we have an entry for theCode, if not create.
+        //        if (!summaryHolder.ContainsKey(theCode))
+        //        {
+        //            SummarizedSalesData ssd = new();
+
+        //            //As we lack a ctor, let's do it in the old way.
+        //            ssd.ProductId = salesData.ProductId;
+        //            ssd.CompanyId = salesData.CompanyId;
+        //            ssd.BrandId = salesData.BrandId;
+        //            ssd.StoreId = salesData.StoreId;
+        //            ssd.WeekNumber = weekNumber;                    
+
+        //            summaryHolder.Add(theCode, ssd);
+        //        }
+
+        //        //Now as we have the required record in the dictionary, we can update volumes and prices.
+        //        summaryHolder[theCode].TotalVolume += salesData.Volume;
+        //        summaryHolder[theCode].TotalPrice += salesData.Price;
+        //    }
+
+        //    //Our work is done. Now return the correct data.
+        //    return summaryHolder.Select(a => a.Value).ToList();
+        //}
 
         private static void SaveToDatabase(IEnumerable<SummarizedSalesData> items)
         {
